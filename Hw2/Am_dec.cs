058a50baf1640859699a7c3f21b90ba7e216e3ff
Acurich <91753821+Acurich@users.noreply.github.com>
2021-11-02 17:37:23 +0300

    Create Am_dec.cs

---
+++ Hw2/Am_dec.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Academy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int postav_pat = int.Parse(Console.ReadLine());
+            Console.WriteLine((postav_pat / 10) % 10);
+            Console.ReadKey();
+        }
+    }
+}
+
