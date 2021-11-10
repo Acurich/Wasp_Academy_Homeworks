d299e98cc47e9f399e6bbd6b6780cb1b835c55b7
Acurich <91753821+Acurich@users.noreply.github.com>
2021-11-10 23:28:52 +0300

    Add files via upload

---
+++ Metro/Metro/Station.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metro
+{
+    class Metro
+    {
+        List<Line> l;
+        string c;
+        public Metro(string city)
+        {
+            this.c = city;
+            l = new List<Line>();
+        }
+        public string GetCity()
+        {
+            return c;
+        }
+        public void AddLine(string name, string color)
+        {
+            Line line = new Line(name, color);
+            l.Add(line);
+        }
+        public void RemoveLine(string name)
+        {
+            foreach (Line i in l)
+            {
+                if (i.GetName() == name)
+                {
+                    l.Remove(i);
+                }
+            }
+        }
+    }
+}
